bbee05130f5c889b80dc0fc15a197eb0fb5512cf
Calvinn Tallent <calvintallentspam@gmail.com>
2021-05-14 20:19:27 -0700

    Temporary changes to try and test a new way of doing things.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const canvas=document.getElementById("canvas");
 const context=canvas.getContext("2d");
-const mazeWidth=10;
-const mazeHeight=10;
+const mazeWidth=11;
+const mazeHeight=11;
 var maze=[];
 
-for(let i=0;i<mazeHeight;i++){
-	maze.push([]);
-	for(let j=0;j<mazeWidth;j++){
-		maze[i].push(1);
-	}
-}
+// for(let i=0;i<mazeHeight;i++){
+// 	maze.push([]);
+// 	for(let j=0;j<mazeWidth;j++){
+// 		maze[i].push(1);
+// 	}
+// }
 
 
 function drawMaze(maze,context){
@@ -29,8 +29,61 @@ function drawMaze(maze,context){
 		}
 	}	
 }
+function drawBetterMaze(maze,context){
+	tileWidth=context.canvas.width/mazeWidth;
+	tileHeight=context.canvas.height/mazeHeight;
+	context.strokeStyle="black";
+	for(var i=0;i<maze.length;i++){
+		for(var j=0;j<maze[i].length;j++){
+			tile=maze[j][i];
+			context.beginPath()//start drawing
+			moveTo(j*tileWidth,i*tileHeight);//move to the top left of the tile	
+			if(tile.up){
+				//if we need to draw the top of the tile, we draw it
+				context.lineTo((j*tileWidth)+tileWidth,i*tileHeight)
+			}else{
+				//else, we just move to the top right corner
+				context.moveTo((j*tileWidth)+tileWidth,i*tileHeight)
+			}
 
-drawMaze(maze,context);
+
+			if(tile.right){
+				//if we need to draw the right side of the tile, draw it
+				context.lineTo((j*tileWidth)+tileWidth,(i*tileHeight)+tileHeight);
+			}else{
+				//else, we move to the bottom right corner of the tile
+				context.moveTo((j*tileWidth)+tileWidth,(i*tileHeight)+tileHeight);
+			}
+
+
+			if(tile.down){
+				context.lineTo(j*tileWidth,(i*tileHeight)+tileHeight);
+			}else{
+				context.moveTo(j*tileWidth,(i*tileHeight)+tileHeight);
+			}
+
+
+			if(tile.left){
+				context.lineTo(j*tileWidth,i*tileHeight);
+			}else{
+				context.moveTo(j*tileWidth,i*tileHeight);
+			}
+			context.stroke();
+		}
+	}	
+}
+
+
+
+var positionX=5;
+var positionY=5;
+var direction=1
+maze=generateGrid(mazeWidth,mazeHeight);
+drawBetterMaze(maze,context);
+
+// var path=new pointer(5,5,1)
+// path.generateMaze();
+// drawMaze(maze,context);
 
 /*
 Griffpatch's algorithim:
@@ -53,3 +106,13 @@ function try directions from(start direction)
 
 
 */
+/*
+	The directions are represented by a number, 
+	with 1 being up, 2 being right, 3 being down,
+	and 4 being left.
+*/
+
+
+
+
+
