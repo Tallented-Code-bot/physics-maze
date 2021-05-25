70d9487444e1aef56d2a32c9db8cf0d5382b37ba
Calvinn Tallent <calvintallentspam@gmail.com>
2021-05-25 10:53:48 -0700

    Implement vector class

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const canvas=document.getElementById("canvas");
 const context=canvas.getContext("2d");
 let LEFT,RIGHT,UP,DOWN;
-
+let balls=[];
 
 class Ball{
 	constructor(x,y,radius){
-		this.x=x;
-		this.y=y;
+		this.position=new Vector(x,y);
+		this.velocity=new Vector(0,0);
+		this.acceleration=new Vector(0,0);
 		this.radius=radius;
+		balls.push(this);
 	}
 	draw() {
 		context.strokeStyle = "black";
 		context.fillStyle = "red";
 		context.beginPath();
-		context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		context.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
 		context.stroke();
 		context.fill();
 	}
@@ -23,19 +25,73 @@ class Ball{
 
 
 
+class Vector{
+	constructor(x,y){
+		this.x=x;
+		this.y=y;
+	}
 
-function keyControl(b){
-	if(UP)b.y--;
-	if(DOWN)b.y++;
-	if(LEFT)b.x--;
-	if(RIGHT)b.x++;
+	getadd(v) {
+		return new Vector(this.x + v.x, this.y + v.y)
+	}
+
+	setadd(v){
+		this.x+=v.x;
+		this.y+=v.y;
+	}
+
+	getsubtract(v) {
+		return new Vector(this.x - v.x, this.y - v.y)
+	}
+
+	setsubtract(v){
+		this.x-=v.x;
+		this.y-=v.y
+	}
+
+	getmag(){
+		return Math.sqrt(this.x**2+this.y**2)
+	}
+
+	getmult(scalar){
+		return new Vector(this.x*scalar,this.y*scalar);
+	}
+
+	setmult(scalar){
+		this.x*=scalar;
+		this.y*=scalar;
+	}
+
+	draw(startPosition,n,color){
+		context.beginPath();
+		context.moveTo(startPosition.x,startPosition.y);
+		context.lineTo(startPosition.getadd(this.getmult(n)).x,startPosition.getadd(this.getmult(n)).y)
+		context.strokeStyle=color;
+		context.stroke();
+	}
+
+
+
 }
 
 
 
+new Ball(200,200,30);
+new Ball(300,300,20);
 
 
+function keyControl(b){
+	if(UP)b.acceleration.y-=0.01;
+	if(DOWN)b.acceleration.y+=0.01;
+	if(LEFT)b.acceleration.x-=0.01;
+	if(RIGHT)b.acceleration.x+=0.01;
 
+	b.velocity.setadd(b.acceleration);
+
+	b.velocity.setmult(0.8);
+
+	b.position.setadd(b.velocity);
+}
 
 
 canvas.addEventListener('keydown',function(e){
@@ -55,16 +111,19 @@ canvas.addEventListener('keyup',function(e){
 		case 68:RIGHT=false;break;
 	}
 })
+function draw(){
+	context.clearRect(0,0,context.canvas.width,context.canvas.height);
+	for(let i=0;i<balls.length;i++){
+		balls[i].draw();	
+		balls[i].velocity.draw(balls[i].position,100,"green");
+		balls[i].acceleration.draw(balls[i].position,10,"black");
+	}
+}
 
 
-
-let ball=new Ball(100,100,20);
-let ball2=new Ball(200,200,10)
 window.requestAnimationFrame(loop);
 function loop(){
-	context.clearRect(0,0,context.canvas.width,context.canvas.height);
-	keyControl(ball);
-	ball.draw();
-	ball2.draw();
+	keyControl(balls[0]);
+	draw();
 	window.requestAnimationFrame(loop);
 }
