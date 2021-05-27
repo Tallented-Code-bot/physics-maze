768cdfbec2eb6bd01790d39cc45443c0bc369924
Calvin Tallent <calvintallentspam@gmail.com>
2021-05-27 13:46:51 +0000

    Finished algorithm, it may still need debugging.

--- testing.js
+++ testing.js
@@ -35,13 +35,29 @@ function generateMaze(x,y){
 
 		for(var l=0;l<4;l++){
 			if(potential[l][0]>-1&&//if the y value is above the minimum
-			  (potential[l][0]<y&&//and the y value is smaller than the maximum
-			  (potential[l][1]>-1&&//and the x value is above the minimum
-			  (potential[l][1]<x&&//and the x value is smaller than the maximum
-			  //todo: finish this if statement
+			  potential[l][0]<y&&//and the y value is smaller than the maximum
+			  potential[l][1]>-1&&//and the x value is above the minimum
+			  potential[l][1]<x&&//and the x value is smaller than the maximum
+			  unvisited[potential[l][0]][potential[l][1]]){//and the neighbor has not been visited
+				neighbors.push(potential[l]);	
+			}
+
+
+			if(neighbors.length){
+				next=neighbors[Math.floor(Math.random()*neighbors.length)];
+				cells[currentCell[0]][currentCell[1]][next[2]]=1;
+				cells[next[0]][next[1]][next[3]]=1;
+				unvisted[next[0]][next[1]]=false;
+				visited++;
+				currentCell=[next[0],next[1]];
+				path.push(currentCell);
+			}else{
+				currentCell=path.pop();
+			}
 		}
 	}
+	return cells;
 
 }
-
-generateMaze(5,5);
+alert("ready to start");
+alert(generateMaze(5,5));
